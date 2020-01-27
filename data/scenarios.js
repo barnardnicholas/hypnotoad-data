871e12b7fe8493a244a0fcae6f2c46f16c1df0d7
Nick <barnard.nicholas@googlemail.com>
2020-01-27 10:19:17 +0000

    initial commit

---
+++ data/scenarios.js
@@ -0,0 +1,43 @@
+const scenarios = [
+  {
+    name: "Italian Birds",
+    slug: "italianbirds",
+    creator_id: 1,
+    color_scheme: ["#561D25", "#CE8147", "#ECDD7B", "#D3E298", "#CDE7BE"],
+    public: true,
+    likes: 0,
+    bg_sounds: [
+      {
+        id: 1,
+        vol: 0.7,
+        pan: 0.5
+      }
+    ],
+    random_sounds: [
+      {
+        id: 2,
+        vol: 0.7,
+        pan: 0.5,
+        freq: 0.5
+      },
+      {
+        id: 3,
+        vol: 0.7,
+        pan: 0.5,
+        freq: 0.5
+      },
+      {
+        id: 4,
+        vol: 0.7,
+        pan: 0.5,
+        freq: 0.5
+      },
+      {
+        id: 5,
+        vol: 0.7,
+        pan: 0.5,
+        freq: 0.5
+      }
+    ]
+  }
+];
