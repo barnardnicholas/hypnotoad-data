d2cc258f3c59c9a997da0335860d92dcdc28885b
Nick <barnard.nicholas@googlemail.com>
2020-01-30 11:38:04 +0000

    data updates

--- data/scenarios.js
+++ data/scenarios.js
@@ -1,43 +1,75 @@
-export default scenarios = [
+const scenarios = [
   {
     name: "Italian Birds",
     slug: "italianbirds",
     creator_id: 1,
     color_scheme: ["#561D25", "#CE8147", "#ECDD7B", "#D3E298", "#CDE7BE"],
-    public: true,
+    is_public: true,
     likes: 0,
-    bg_sounds: [
+    sounds: [
       {
         id: 1,
-        vol: 0.7,
+        volume: 0.7,
         pan: 0.5
-      }
-    ],
-    random_sounds: [
+      },
       {
         id: 2,
-        vol: 0.7,
-        pan: 0.5,
-        freq: 0.5
+        volume: 0.6,
+        pan: 0.4,
+        frequency: 0.3
       },
       {
         id: 3,
-        vol: 0.7,
-        pan: 0.5,
-        freq: 0.5
+        volume: 0.4,
+        pan: 0.2,
+        frequency: 0.6
       },
       {
         id: 4,
-        vol: 0.7,
-        pan: 0.5,
-        freq: 0.5
+        volume: 0.8,
+        pan: 0.8,
+        frequency: 0.4
       },
       {
         id: 5,
-        vol: 0.7,
-        pan: 0.5,
-        freq: 0.5
+        volume: 0.1,
+        pan: 0.1,
+        frequency: 0.1
       }
+    ]
+  },
+  {
+    name: "Docks",
+    slug: "docks",
+    creator_id: 2,
+    color_scheme: ["#561D25", "#CE8147", "#ECDD7B", "#D3E298", "#CDE7BE"],
+    is_public: true,
+    likes: 0,
+    sounds: [
+      {
+        id: 6,
+        volume: 0.7,
+        pan: 0.3
+      },
+      {
+        id: 7,
+        volume: 0.3,
+        pan: 0.7
+      },
+      {
+        id: 8,
+        volume: 0.6,
+        pan: 0.2,
+        frequency: 0.6
+      }
+      // {
+      //   id: 9,
+      //   volume: 0.5,
+      //   pan: 0.8,
+      //   frequency: 0.2
+      // }
     ]
   }
 ];
+
+export default scenarios;
--- data/sounds.js
+++ data/sounds.js
@@ -5,8 +5,7 @@ const sounds = [
     slug: "italianbirds",
     type: "background",
     loop: true,
-    urls: ["italian-birds.ogg"],
-    sprite: {}
+    urls: ["italianbirds"]
   },
   {
     id: 2,
@@ -15,28 +14,22 @@ const sounds = [
     type: "random",
     loop: false,
     urls: [
-      "blackbird-sprite.ogg",
-      "blackbird-sprite.m4a",
-      "blackbird-sprite.mp3",
-      "blackbird-sprite.ac3"
-    ],
-    sprite: {
-      blackbird1: [0, 3342.2222222222226],
-      blackbird2: [5000, 2602.0861678004535],
-      blackbird3: [9000, 2793.650793650794],
-      blackbird5: [13000, 3789.2063492063494],
-      blackbird6: [18000, 2099.9546485260757],
-      blackbird7: [22000, 2515.011337868479],
-      blackbird8: [26000, 3121.6326530612264],
-      blackbird9: [31000, 2935.873015873014],
-      blackbird10: [35000, 4482.902494331064],
-      blackbird11: [41000, 4009.795918367345],
-      blackbird12: [47000, 3243.537414965985],
-      blackbird13: [52000, 3112.9251700680243],
-      blackbird14: [57000, 4018.5034013605473],
-      blackbird15: [63000, 2950.385487528351],
-      blackbird16: [67000, 2973.605442176876]
-    }
+      "blackbird1",
+      "blackbird2",
+      "blackbird3",
+      "blackbird5",
+      "blackbird6",
+      "blackbird7",
+      "blackbird8",
+      "blackbird9",
+      "blackbird10",
+      "blackbird11",
+      "blackbird12",
+      "blackbird13",
+      "blackbird14",
+      "blackbird15",
+      "blackbird16"
+    ]
   },
   {
     id: 3,
@@ -45,18 +38,12 @@ const sounds = [
     type: "random",
     loop: false,
     urls: [
-      "song-thrush-sprite.ogg",
-      "song-thrush-sprite.m4a",
-      "song-thrush-sprite.mp3",
-      "song-thrush-sprite.ac3"
-    ],
-    sprite: {
-      "song thrush 1": [0, 2187.02947845805],
-      "song thrush 2": [4000, 2175.4195011337865],
-      "song thrush 3": [8000, 1264.0362811791376],
-      "song thrush 4": [11000, 3922.721088435374],
-      "song thrush 5": [16000, 3388.6621315192756]
-    }
+      "songthrush1",
+      "songthrush2",
+      "songthrush3",
+      "songthrush4",
+      "songthrush5"
+    ]
   },
   {
     id: 4,
@@ -65,19 +52,14 @@ const sounds = [
     type: "random",
     loop: false,
     urls: [
-      "cuckoo-sprite.ogg",
-      "cuckoo-sprite.m4a",
-      "cuckoo-sprite.mp3",
-      "cuckoo-sprite.ac3"
-    ],
-    sprite: {
-      "cuckoo 2": [0, 2314.739229024943],
-      "cuckoo 3": [4000, 3847.2562358276646],
-      "cuckoo 4": [9000, 642.9024943310661],
-      "cuckoo 5": [11000, 2082.5396825396824],
-      "cuckoo 6": [15000, 3710.8390022675726],
-      "cuckoo 7": [20000, 5214.331065759638]
-    }
+      "cuckoo1",
+      "cuckoo2",
+      "cuckoo3",
+      "cuckoo4",
+      "cuckoo5",
+      "cuckoo6",
+      "cuckoo7"
+    ]
   },
   {
     id: 5,
@@ -86,20 +68,65 @@ const sounds = [
     type: "random",
     loop: false,
     urls: [
-      "chaffinch-sprite.ogg",
-      "chaffinch-sprite.m4a",
-      "chaffinch-sprite.mp3",
-      "chaffinch-sprite.ac3"
-    ],
-    sprite: {
-      chaffinch: [0, 2201.541950113379],
-      chaffinch2: [4000, 2277.0068027210878],
-      "chaffinch 3": [8000, 1893.8775510204077],
-      "chaffinch 4": [11000, 2555.6462585034014],
-      "chaffinch 5": [15000, 3417.6870748299316],
-      "chaffinch 6": [20000, 2320.5442176870756]
-    }
+      "chaffinch1",
+      "chaffinch2",
+      "chaffinch3",
+      "chaffinch 4",
+      "chaffinch 5",
+      "chaffinch 6"
+    ]
+  },
+  {
+    id: 6,
+    name: "Docks",
+    slug: "docks",
+    type: "background",
+    loop: true,
+    urls: ["docks"]
+  },
+  {
+    id: 7,
+    name: "City Rumbling",
+    slug: "cityrumbling",
+    type: "background",
+    loop: true,
+    urls: ["cityrumbling"]
+  },
+  {
+    id: 8,
+    name: "Seagull",
+    slug: "seagull",
+    type: "random",
+    loop: false,
+    urls: [
+      "seagull1",
+      "seagull2",
+      "seagull3",
+      "seagull4",
+      "seagull5",
+      "seagull6",
+      "seagull7",
+      "seagull8",
+      "seagull9",
+      "seagull10",
+      "seagull11",
+      "seagull12",
+      "seagull13",
+      "seagull14",
+      "gull1",
+      "gull2",
+      "gull3",
+      "seagullsflap"
+    ]
+  },
+  {
+    id: 9,
+    name: "Ship Oars",
+    slug: "shipoars",
+    type: "random",
+    loop: false,
+    urls: ["oarinwater1", "oarinwater2", "oarsinboat"]
   }
 ];
 
-module.exports = { sounds };
+export default sounds;
